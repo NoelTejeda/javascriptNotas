ae1b67a46365073a1f2d8a6997b41cbbdc7a8687
noel <noelati@gmail.com>
2022-07-18 22:39:23 -0400

    modificaciones

--- slice.js
+++ slice.js
@@ -30,9 +30,6 @@ Metodos String:
 * substr(start, end)  == slice con la diferencia (start, longitud) empieza y la cantidad de caracteres a imprimir. sino definimos hasta
                             donde, imprimirá hasta el final.
 
-
-
-
 slice no modifica el array original.(no hay mutación)
 splice SI modifica el array original.(hay mutación)
 */
