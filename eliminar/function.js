3bf73a8abbe7cd1d50fb01f57746ff845f906481
Noel <noelati@gmail.com>
2023-05-25 22:42:55 -0400

    se elimina algunos archivos no útiles

---
+++ eliminar/function.js
@@ -0,0 +1,7 @@
+function sumar(){
+
+  let num1 = Number(document.getElementById("num1").value)
+  let num2 = Number(document.getElementById("num2").value)
+
+  document.getElementById("resultado").value ="la suma es: " +(num1 + num2)
+}
